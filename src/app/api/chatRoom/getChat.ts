1d2a88516274c72cb86e2df0188bb4968c241792
i-nafrizaldi <ivannafrizaldi@gmail.com>
2024-10-09 22:14:40 +0700

    feat: env

--- src/app/api/chatRoom/getChat.ts
+++ src/app/api/chatRoom/getChat.ts
@@ -1,8 +1,9 @@
+import { BASE_API_URL } from "@/utils/config";
 import axios from "axios";
 
 export const getChat = async () => {
   try {
-    const res = await axios.get("http://localhost:2000/results");
+    const res = await axios.get(`${BASE_API_URL}`);
     return res.data;
   } catch (error) {
     console.log(error);
