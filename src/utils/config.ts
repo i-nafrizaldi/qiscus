5bb13348bd472e54016f13e39e3eac5b9cd6129d
Ivan Nafrizaldi <142771799+i-nafrizaldi@users.noreply.github.com>
2024-10-09 22:16:35 +0700

    feat: env (#4)
    * feat: env
    
    * Update vercel.json

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,15 @@
-export const appConfig = {
-  chatUrl: "http://localhost:3000/",
-  // "https://gist.githubusercontent.com/asharijuang/23745f3132fa30e666db68d2bf574e4a/raw/5d556dbb9c2aea9fdf3e1ec96e45f62a88cea7b6/chat_response.json",
-};
+import { config } from "dotenv";
+import { resolve } from "path";
 
+export const NODE_ENV = process.env.NODE_ENV || "development";
 
+const envFile = NODE_ENV === "development" ? ".env.development" : ".env";
+
+config({ path: resolve(__dirname, `../${envFile}`) });
+config({ path: resolve(__dirname, `../${envFile}.local`), override: true });
+
+// Load all environment variables from .env file
+
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const PORT = process.env.PORT || 3000;
